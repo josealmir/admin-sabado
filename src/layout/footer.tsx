0e661ec4fe96d0bd851a52590005f1cb412d1436
jose-almir <almir.filho@microcashif.com.br>
2026-01-18 16:41:31 -0300

    Updates project

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
     return (
         <footer className="bg-link-light text-center text-lg-start mt-auto">
             <div className="text-center p-3">
-                © 2024 Teste Admin. Todos os direitos reservados.
+                © 2024 Start Bootstrap. Todos os direitos reservados.
             </div>
         </footer>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,38 @@
 import { Button, Col, Form, Row, Spinner } from "react-bootstrap"
 import { Main } from "../layout/main"
 import { Sidebar } from "../layout/sidebar"
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { ViaCepApi } from "../app/apis/viacep.api";
 import { SidebarItems } from "../layout/infra/sidebar.infra";
 import { Body } from "../layout/body";
+import type { ViaCepResponse } from "../app/apis/models/responses/viacep.response";
 
-export const  IndexPage = () => {
+
+export const IndexPage = () => {
 
     const [pending, setPending] = useState(false);
     const [cep, setCep] = useState("");
+    const [response, setResponse] = useState<ViaCepResponse | null>(null);
+    const [ceps, setCeps] = useState<ViaCepResponse[]>([]);
+
+
+    // Exemplo prático de uso do useMemo para filtrar ceps do estado de SP
+    const cepsFiltrados = useMemo(() => {
+        return ceps.filter(item => item.uf === "SP");
+    }, [ceps]);
+
 
     const onSearchCep = () => {
-     setPending(true);
-     ViaCepApi.getAddressByCep(cep)
-     .then((response) => {
-        console.log(response);
-     }).finally(() => {
-        setPending(false);
-     });
-    
+        setPending(true);
+        ViaCepApi.getAddressByCep(cep)
+            .then((response) => {
+                setCeps(prev => [...prev, response]);
+                setResponse(response);
+            }).finally(() => {
+                setPending(false);
+            });
     }
+
 
     return (
         <>
@@ -28,24 +40,35 @@ export const  IndexPage = () => {
                 <Sidebar itens={SidebarItems} size={250} />
                 <Main>
                     <h1 className="mt-4">Pesquise o seu cep aqui !</h1>
-                    <Form  action={onSearchCep}>
+                    <Form action={onSearchCep}>
                         <Row>
                             <Col sm={10}>
-                                <Form.Control size="lg" type="text" 
-                                name="cep"
-                                placeholder="Informa o seu cep"
-                                onChange={(e) => setCep(e.target.value)}
-                                value={cep}  />
+                                <Form.Control size="lg" type="text"
+                                    name="cep"
+                                    placeholder="Informa o seu cep"
+                                    onChange={(e) => setCep(e.target.value)}
+                                    value={cep} />
                             </Col>
                             <Col sm={2}>
-                                <Button 
+                                <Button
                                     type="submit"
                                     size="lg"
                                     variant="primary"
                                     disabled={pending}
-                                > { pending ? <span> <Spinner animation="border" size="sm" /> Pesquiando </span>: "Pesquisar"}</Button>
+                                > {pending ? <span> <Spinner animation="border" size="sm" /> Pesquiando </span> : "Pesquisar"}</Button>
                             </Col>
-                            { }
+
+                            {
+                                cepsFiltrados.map((address) => {
+                                    return (<Row key={address.cep} className="mt-4">
+                                        <Col><strong>CEP:</strong> {address.cep} </Col>
+                                        <Col><strong>Logradouro:</strong> {address.logradouro} </Col>
+                                        <Col><strong>Bairro:</strong> {address.bairro} </Col>
+                                        <Col><strong>Cidade:</strong> {address.localidade} </Col>
+                                        <Col><strong>Estado:</strong> {address.uf} </Col>
+                                    </Row>)
+                                })
+                            }
                         </Row>
                     </Form>
                 </Main>
